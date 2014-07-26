214fb3782965427f7f7bbb4a703aee57b6eb55e7
stinga11 <stinga11@hotmail.com>
2014-07-26 11:26:56 -0400

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,4 +1,7 @@
-$(call inherit-product, device/samsung/quincyatt/full_quincyatt.mk)
+$(call inherit-product, device/samsung/quincytmo/full_quincytmo.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
@@ -6,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-I717 TARGET_DEVICE=SGH-I717 BUILD_FINGERPRINT="samsung/SGH-I717/SGH-I717:4.1.2/JZO54K/I717UCMD3:user/release-keys" PRIVATE_BUILD_DESC="SGH-I717-user 4.1.2 JZO54K I717UCMD3 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=SGH-T879 TARGET_DEVICE=SGH-T879 BUILD_FINGERPRINT="samsung/SGH-T879/SGH-T879:4.0.4/IMM76D/UVMA1:user/release-keys" PRIVATE_BUILD_DESC="SGH-T879-user 4.0.4 IMM76D UVMA1 release-keys"
 
-PRODUCT_NAME := cm_quincyatt
-PRODUCT_DEVICE := quincyatt
+PRODUCT_NAME := cm_quincytmo
+PRODUCT_DEVICE := quincytmo
